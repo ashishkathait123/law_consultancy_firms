b62c0790a31378f921ff7262680effbe7b1d6a0b
ashish kathait <ashish2kathait2gmail.com>
2025-06-21 18:42:48 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    outDir: 'build', // 👈 This is what Vercel wants
+    chunkSizeWarningLimit: 1000 // optional: to suppress 500kB warning
+  }
 })
